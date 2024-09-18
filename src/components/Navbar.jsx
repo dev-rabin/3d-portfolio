fe5dfc84aa6f72f8e7145c96abff6c146ae17192
dev-rabin <robinmandhotia@gmail.com>
2024-09-18 20:40:42 +0530

    setup folder, @react-three/fiber, @react-drei

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+   <header className='header'>
+    <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+    <p className='blue-gradient_text'>SY</p>
+    </NavLink>
+    <NavLink to="/about" className={({isActive}) => isActive ? "text-blue-500" : "text-black"}>
+        About
+    </NavLink>
+    <NavLink to="/projects" className={({isActive}) => isActive ? "text-blue-500" : "text-black"}>
+        Projects
+    </NavLink>
+   </header>
+  )
+}
+
+export default Navbar
