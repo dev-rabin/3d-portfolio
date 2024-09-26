41c7a9d1a44b256b9c2276f883a268bbbcb6ed8f
dev-rabin <robinmandhotia@gmail.com>
2024-09-26 21:23:10 +0530

    set loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,13 +3,13 @@ import { Html } from '@react-three/drei'
 
 const Loader = () => {
     return (
-        <Html>
-            <div className='flex justify-center items-center'>
-                <div className='w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin' />
+        <Html center>
+            <div className='fixed inset-0 flex justify-center items-center'>
+                <div className='w-20 h-20 border-4 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin' />
             </div>
         </Html>
-
     )
 }
 
 export default Loader
+
