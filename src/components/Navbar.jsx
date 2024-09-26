da19c1a23678a468dce37a26dbe81b8f473dc78d
dev-rabin <robinmandhotia@gmail.com>
2024-09-26 13:25:09 +0530

    about page complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,14 @@ const Navbar = () => {
     <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
     <p className='blue-gradient_text'>SY</p>
     </NavLink>
-    <NavLink to="/about" className={({isActive}) => isActive ? "text-blue-500" : "text-black"}>
-        About
-    </NavLink>
-    <NavLink to="/projects" className={({isActive}) => isActive ? "text-blue-500" : "text-black"}>
-        Projects
-    </NavLink>
+    <nav className='flex text-lg gap-7 font-medium'>
+        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
+          About
+        </NavLink>
+        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
+          Projects
+        </NavLink>
+      </nav>
    </header>
   )
 }
